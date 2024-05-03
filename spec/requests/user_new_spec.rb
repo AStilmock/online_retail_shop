c74cb3bc5f878f7e9d13418bd382bc524e0fd371
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-02 20:17:21 -0600

    sorcery install troubleshooting updates

---
+++ spec/requests/user_new_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'User Registration Process' do
+  describe 'user can create account' do
+    it 'user#new' do
+      visit new_user_path
+      expect(page).to have_content('Create an Account')
+      expect(page).to have_field('email')
+      expect(page).to have_field('password')
+      expect(page).to have_field('password_confirmation')
+    end
+
+    it 'user#create' do
+      visit new_user_path
+      fill_in 'email', with: 'testemail@email.com'
+      fill_in 'password', with: 'testpassword'
+      fill_in 'password_confirmation', with: 'testpassword'
+      click_button 'Create Account'
+      expect(current_path).to eq(users_path)
+    end
+  end
+end
