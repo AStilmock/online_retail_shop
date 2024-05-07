ce4c30882a99261c5474b285518ffde9095303f4
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-07 11:01:58 -0600

    sorcery troubleshooting updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(email: params[:email], crypted_password: params[:password], salt: params[:salt], permission: params[:permission], discount: params[:discount])
     require 'pry'; binding.pry
-    @user = User.new(user_params)
     if @user.save
+      # Create user from email and password entered in form
+      # If user is created, create new session with user as session
+      # Create new User object setting defaults for permission and discount
+      # Password is encrypted at login using password, salt and bcrypt to get crypted_password
       auto_login(@user)
       redirect_to(:users, notice: 'User created')
     else
@@ -21,6 +25,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :permission, :discount, :salt)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
