8708f5f87ce9b759c038cdba02bc89e02283a8ea
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-18 15:25:55 -0600

    Added Discount model association and validation testing

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,10 @@
 class Discount < ApplicationRecord
-  
+  validates_presence_of :name, :category, :description, :price_percent, :amount
+
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  has_many :users, through: :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :sale_invoices
+  has_many :sales, through: :sale_invoices
 end
