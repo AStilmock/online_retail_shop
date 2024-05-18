79e51d42998c2a68abec60493eff4a0747d91c6f
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-18 12:17:53 -0600

    Added Vendor model validation and association testing

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,6 @@
 class Vendor < ApplicationRecord
-  
+  has_many :items
+  has_many :purchase_orders
+
+  validates_presence_of :name, :description, :address, :email, :phone
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
   describe 'validations' do 
-
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone) }
   end
 
   describe 'associations' do  
-    
+    it { should have_many(:items)}
+    it { should have_many(:purchase_orders)}
   end
 end
