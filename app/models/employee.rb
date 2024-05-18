6ccad79b686b7ab8a91a4d8f4cd1655235b5ba01
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-18 12:15:32 -0600

    Added Employee model validation and association testing

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ApplicationRecord
-  
+  belongs_to :user
+
+  validates_presence_of :first_name, :last_name, :address, :email, :phone, :category
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
   describe 'validations' do 
-
+    it { should validate_presence_of(:first_name)}
+    it { should validate_presence_of(:last_name)}
+    it { should validate_presence_of(:address)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:phone)}
+    it { should validate_presence_of(:category)}
   end
 
   describe 'associations' do  
-    
+    it { should belong_to(:user) }
   end
 end
