56957c2594d4c11ea34b6083ae918e483aa1dfab
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-18 15:19:45 -0600

    Added updates for Item model association and validation testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
-  
+  validates_presence_of :name, :description, :unit_price, :item_cost, :quantity, :in_stock
+
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :users, through: :invoice_items
+  has_many :discounts, through: :invoice_items
+  has_many :purchase_orders
+
+  belongs_to :product_category
+  belongs_to :vendor
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe 'validations' do 
-
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_presence_of(:item_cost) }
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:in_stock) }
   end
 
   describe 'associations' do  
-    
+    it { should have_many(:invoice_items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:users).through(:invoice_items)}
+    it { should have_many(:discounts).through(:invoice_items)}
+    it { should have_many(:purchase_orders)}
+
+    it { should belong_to(:product_category)}
+    it { should belong_to(:vendor)}
   end
 end
