51fb350b5d763a4d084bee186b70b76e87a15482
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-05-18 20:36:20 -0600

    Added updates for user/shopping index page

--- app/controllers/user/shopping_controller.rb
+++ app/controllers/user/shopping_controller.rb
@@ -1,8 +1,9 @@
-class ShoppingController < ApplicationController
+class User::ShoppingController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index
-    @items = Item.all
+    @user = current_user
     @categories = ProductCategory.all
+    @items = Item.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   resources :users
   resources :categories, only: [:index, :show]
   resources :items, only: [:index, :show]
+  
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
   get '/shopping', to: 'shopping#index'
+  get '/users/:id/shopping', to: 'user/shopping#index', as: 'user_shopping'
 end
