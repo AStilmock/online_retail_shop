7dea8bb4d199f1c9a505dc85c59dea8a09e0c8d5
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-12-19 19:10:05 -0700

    Home page stimulus updates

--- app/javascript/controllers/format_controller.js
+++ app/javascript/controllers/format_controller.js
@@ -1,11 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["link"];
+  static targets = ["link", "message"];
 
   connect() {
+    // Set default link color for all link targets
     this.linkTargets.forEach((link) => {
-      link.style.color = "blue"; // Set default link color
+      link.style.color = "blue";
     });
   }
+
+  formatMessage() {
+    // Update the content and styles of the message target
+    this.messageTarget.innerHTML = `
+      <h1 class="text-4xl font-bold text-center text-blue-600">
+        WELCOME TO THE TEST ONLINE RETAIL STORE!
+      </h1>
+      <p class="text-base text-gray-600 text-center">
+        All sales are final. And all merchandise is fake.
+      </p>
+    `;
+  }
 }
+
