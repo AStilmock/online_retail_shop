3d5d0f6ad980e26ff5abb9cb58fa3a05f0d2fac3
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-12-22 20:23:53 -0600

    Notation updates for login/logout path

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,9 +7,9 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to(root_path, notice: 'Login successful')
+      redirect_back_or_to(root_path, notice: 'Login successful') #text not displaying after successful login
     else
-      flash.now[:alert] = 'Login failed'
+      flash.now[:alert] = 'Login failed' #text not displaying after failed login
       render action: 'new'
     end
   end
