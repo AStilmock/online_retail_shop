786bdf30cf1fe241b632a5f000475bfe5a0f3e8c
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-12-27 21:27:56 -0700

    updates for shopping index view page

--- app/javascript/controllers/shopping_controller.js
+++ app/javascript/controllers/shopping_controller.js
@@ -1,1 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["link", "message"];
+
+  connect() {
+    this.linkTargets.forEach((link) => {
+      link.style.color = "blue";  // Default color for links
+    });
+
+    if (this.hasMessageTarget) {
+      this.formatMessage();
+    }
+  }
+
+  formatMessage() {
+    this.messageTaget.innerHTML = `
+      <h3 class="text-4xl font-bold text-center text-blue-600"> Here be the shopping page - 
+      view our inventory selection below! </h3>
+    `;
+  }
+}
